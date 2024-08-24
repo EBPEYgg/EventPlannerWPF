54c69bf8724a8acb1adfb6b741fa0214b5b2c9dd
Alexey <com.twin@yandex.ru>
2024-08-25 01:11:36 +0700

    feature(ViewModel): add navigation

---
+++ src/EventPlannerWPF.ViewModel/Services/NavigationService.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EventPlannerWPF.ViewModel.Services
+{
+    public static class NavigationService
+    {
+        public static Action<ObservableObject> Navigate { get; set; } = _ => { };
+
+        public static void NavigateTo<TViewModel>() where TViewModel : ObservableObject, new()
+        {
+            var viewModel = new TViewModel();
+            Navigate(viewModel);
+        }
+    }
+}
