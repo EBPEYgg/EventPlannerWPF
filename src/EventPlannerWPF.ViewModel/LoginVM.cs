1eb95a8e479327c6f1cceaddc9d5253eefb701b8
Alexey <com.twin@yandex.ru>
2024-08-27 23:13:30 +0700

    feature(View & ViewModel): add user authorization

--- src/EventPlannerWPF.ViewModel/LoginVM.cs
+++ src/EventPlannerWPF.ViewModel/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using EventPlannerWPF.Model.Classes;
 using EventPlannerWPF.Model.Data;
 using EventPlannerWPF.ViewModel.Services;
 using Microsoft.EntityFrameworkCore;
@@ -8,49 +9,83 @@ namespace EventPlannerWPF.ViewModel
 {
     public partial class LoginVM : ObservableObject
     {
+        #region Observable Property
+        /// <summary>
+        /// Возвращает и задает логин пользователя в соответствующем TextBox.
+        /// </summary>
         [ObservableProperty]
         private string _loginText;
 
+        /// <summary>
+        /// Возвращает и задает пароль пользователя в соответствующем TextBox.
+        /// </summary>
         [ObservableProperty]
         private string _passwordText;
 
+        /// <summary>
+        /// Возвращает и задает текст ошибки при неудачной авторизации (для теста).
+        /// </summary>
         [ObservableProperty]
         private string _errorMessage;
+        #endregion
 
+        /// <summary>
+        /// Контекст данных для БД.
+        /// </summary>
         private readonly EventPlannerContext db;
 
         public LoginVM()
         {
             db = new EventPlannerContext();
-            db.Database.OpenConnectionAsync().Wait();
         }
 
+        #region Commands
         [RelayCommand]
-        private void Login()
+        private async Task Login()
         {
-            var currentUser = db.User.FirstOrDefault(u => u.Login == LoginText && u.Password == PasswordText);
-
-            if (currentUser != null)
+            if (string.IsNullOrEmpty(LoginText) || string.IsNullOrEmpty(PasswordText))
             {
-                NavigationService.NavigateTo<MainVM>();
-                db.Database.CloseConnectionAsync().Wait();
+                ErrorMessage = "Please enter login and password";
                 return;
             }
 
-            ErrorMessage = "error";
+            var currentUser = await db.User
+                .FirstOrDefaultAsync(u => u.Login == LoginText && u.Password == PasswordText);
+
+            if (currentUser != null)
+            {
+                UserSession.Instance.SetUser(currentUser);
+                NavigationService.NavigateTo<MainVM>();
+                return;
+            }
+
+            ErrorMessage = "Invalid login or password";
         }
 
         [RelayCommand]
         private void Register()
         {
+            ClearTextBoxes();
             NavigationService.NavigateTo<RegisterVM>();
         }
 
         [RelayCommand]
         private void Quit()
         {
-            db.Database.CloseConnectionAsync().Wait();
             Environment.Exit(0);
         }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Метод, который очищает значения в текстовых полях.
+        /// </summary>
+        private void ClearTextBoxes()
+        {
+            LoginText = string.Empty;
+            PasswordText = string.Empty;
+            ErrorMessage = string.Empty;
+        }
+        #endregion
     }
 }
